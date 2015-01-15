4ffacddd230c848e41df832039ba70f2d33ba0c9
Evyatar Reiss <evyatar16@meet.mit.edu>
2015-01-15 20:04:48 +0200

    OOP.py

---
+++ OOP.py
@@ -0,0 +1,12 @@
+class Animal: 
+	def __init__(self,name,age,color,size):
+		self.name=name
+		self.age=age
+		self.color=color
+		self.size=size
+
+cat=Animal("evyatar",45,"blue","big")
+print(cat.name)
+print(cat.age)
+print(cat.color)
+print(cat.size)
