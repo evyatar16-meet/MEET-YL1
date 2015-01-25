bab69f501709802e701fc01b94f976d971502ed8
Evyatar Reiss <evyatar16@meet.mit.edu>
2015-01-25 19:50:29 +0200

    zoolab1

--- OOP.py
+++ OOP.py
@@ -5,8 +5,18 @@ class Animal:
 		self.color=color
 		self.size=size
 
-cat=Animal("evyatar",45,"blue","big")
-print(cat.name)
-print(cat.age)
-print(cat.color)
-print(cat.size)
+	def print_all(self):
+		print(self.name)
+		print(self.age)
+		print(self.color)
+		print(self.size)
+
+	def sleep(self,hours,dream):
+		print("The Animal ",self.name," is dreaming for ", hours ,"of ", dream)
+		
+
+dog = Animal("Evyatar",45,"blue","tiny")
+lion = Animal("Reiss",1,"orang","big")
+dog.print_all()
+lion.print_all()
+lion.sleep(23,"python")
